17492ae0bb791663f25e38555a8698354315e7c4
xianglid3 <51499804+xianglid3@users.noreply.github.com>
2026-06-14 18:54:44 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //2 pointer instead of stack
+
+
+        int i = 0; 
+        int j = s.length() - 1; 
+
+        while(i < j){
+
+            while( i<j && !Character.isLetterOrDigit(s.charAt(i)) ){
+                i++;
+            }
+
+            while(i<j && !Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+            }
+
+
+            char left = Character.toLowerCase(s.charAt(i));
+            char right = Character.toLowerCase(s.charAt(j));
+
+            if(left != right){
+                return false;
+            }
+
+            i++;
+            j--;
+    
+        }
+
+        return true;
+    }
+}
